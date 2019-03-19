e9929b3851ba3e2dcdd04e5acf693591ce0bb7a9
andres <andrescorr01@hotmail.com>
2019-03-19 16:33:27 -0600

    programas de entradas de datos
    programa de entrada y salida de datos

---
+++ entradapersonal.py
@@ -0,0 +1,12 @@
+ #capturamos la entrada de teclado y la guardamos en una variable 
+nombre = input("ingresar nombre: ")
+apellido = input("ingresar su apellido: ")
+edad = input("ingresar su edad: ")
+
+#ingresamos lo capturado por teclado
+print( f"bienvenido {nombre} {apellido},su edad es:{edad}")
+#imprimimos lo capturado poir teclado
+numero1 = int(input("ingrese un numero:"))
+numero2 = int(input("ingrese otro numero:"))
+resultado = numero1 + numero2
+print(f"el resultado es{resultado}")
